0b1377594be520499edecea409c19ca798f19082
Marc Neumann <marc.nemn@gmail.com>
2018-01-04 15:44:36 +0100

    Updated note items.

--- app/src/main/java/com/notes/marcnmn/pandamarkdownnotes/ui/page/home/notes/NotesAdapter.kt
+++ app/src/main/java/com/notes/marcnmn/pandamarkdownnotes/ui/page/home/notes/NotesAdapter.kt
@@ -1,8 +1,11 @@
 package com.notes.marcnmn.pandamarkdownnotes.ui.page.home.notes
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.notes.marcnmn.pandamarkdownnotes.R
 import com.notes.marcnmn.pandamarkdownnotes.model.Note
 import javax.inject.Inject
 
@@ -24,14 +27,15 @@ class NotesAdapter @Inject constructor() : RecyclerView.Adapter<NotesAdapter.Vie
         selectedListener = s
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        return ViewHolder(TextView(parent?.context))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.view_notes_item, parent, false)
+        return ViewHolder(v)
     }
 
     override fun onBindViewHolder(h: ViewHolder?, p: Int) {
         if (h == null || p < 0 || p >= items.size) return
-        h.text.text = items[p].raw
-        h.text.setOnClickListener {
+        h.updateModel(items[p])
+        h.itemView.setOnClickListener {
             val pos = h.adapterPosition
             if (pos >= 0 && pos < items.size) selectedListener?.selected(items[pos])
         }
@@ -39,7 +43,17 @@ class NotesAdapter @Inject constructor() : RecyclerView.Adapter<NotesAdapter.Vie
 
     override fun getItemCount(): Int = items.size
 
-    inner class ViewHolder(val text: TextView) : RecyclerView.ViewHolder(text)
+    inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        private var title = v.findViewById<TextView>(R.id.title)
+        private var content = v.findViewById<TextView>(R.id.content)
+        private var edited = v.findViewById<TextView>(R.id.last_edited)
+
+        fun updateModel(n: Note) {
+            title.text = n.raw
+            content.text = n.raw
+            edited.text = "2W"
+        }
+    }
 
     interface NoteSelected {
         fun selected(n: Note)
