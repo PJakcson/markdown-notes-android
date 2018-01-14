99caf8628b60fbd04130d798300a18fd52f0ae3e
Marc Neumann <marc.nemn@gmail.com>
2018-01-14 20:07:19 +0100

    Added fingerprint dialog fragment.

--- app/src/main/java/com/notes/marcnmn/pandamarkdownnotes/ui/dialog/FingerprintFragment.kt
+++ app/src/main/java/com/notes/marcnmn/pandamarkdownnotes/ui/dialog/FingerprintFragment.kt
@@ -1,13 +1,16 @@
 package com.notes.marcnmn.pandamarkdownnotes.ui.dialog
 
-import android.app.Dialog
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.DialogFragment
 import android.support.v4.hardware.fingerprint.FingerprintManagerCompat
-import android.support.v7.app.AlertDialog
+import android.support.v4.os.CancellationSignal
 import android.view.LayoutInflater
-import android.widget.FrameLayout
+import android.view.View
+import android.view.ViewGroup
 import com.notes.marcnmn.pandamarkdownnotes.R
+import kotlinx.android.synthetic.main.view_fingerprint_dialog.*
 
 
 /*
@@ -15,45 +18,46 @@ import com.notes.marcnmn.pandamarkdownnotes.R
  */
 
 class FingerprintFragment : DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val fl = FrameLayout(context!!)
-        LayoutInflater.from(context!!).inflate(R.layout.view_fingerprint_dialog, fl)
+    private val cs = CancellationSignal()
+    private lateinit var fm: FingerprintManagerCompat
 
-        val builder = AlertDialog.Builder(activity!!)
-        builder.setView(fl)
-                .setIcon(R.drawable.ic_fingerprint)
-                .setNegativeButton("Abbrechen", { _, _ -> })
-        val d = builder.create()
-        setupFingerprint(d)
-
-        return d
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        retainInstance = true
+        fm = FingerprintManagerCompat.from(activity!!)
+        fm.authenticate(null, 0, cs, object : FingerprintManagerCompat.AuthenticationCallback() {
+            override fun onAuthenticationError(errMsgId: Int, errString: CharSequence?) = handleError(errMsgId, errString)
+            override fun onAuthenticationSucceeded(result: FingerprintManagerCompat.AuthenticationResult?) = handleSuccess()
+            override fun onAuthenticationHelp(helpMsgId: Int, helpString: CharSequence?) = handleHelp(helpString)
+            override fun onAuthenticationFailed() = handleFailed()
+        }, null)
+        return LayoutInflater.from(context!!).inflate(R.layout.view_fingerprint_dialog, container)
     }
 
-    private fun setupFingerprint(d: Dialog) {
-        val fm = FingerprintManagerCompat.from(activity!!)
+    private fun handleError(id: Int, msg: CharSequence?) {
+        println("Fingerprint error: $id - $msg")
+    }
 
-//        if (fm.isHardwareDetected)
-//            Snackbar.make(view, "no hardware", Snackbar.LENGTH_LONG).show()
-//
-//        if (!fm.hasEnrolledFingerprints())
-//            Snackbar.make(view, "no fingerprints", Snackbar.LENGTH_LONG).show()
+    private fun handleSuccess() {
+        println("Fingerprint success")
+        dismiss()
+    }
 
-        fm.authenticate(null, 0, null, object : FingerprintManagerCompat.AuthenticationCallback() {
-            override fun onAuthenticationError(errMsgId: Int, errString: CharSequence?)
-                    = logAndDismiss("onAuthenticationError")
+    private fun handleHelp(msg: CharSequence?) {
+        println("Fingerprint help: $msg")
+        if (view == null || msg == null) return
+        Snackbar.make(view!!, msg, Snackbar.LENGTH_LONG).show()
+    }
 
-            override fun onAuthenticationSucceeded(result: FingerprintManagerCompat.AuthenticationResult?)
-                    = logAndDismiss("onAuthenticationSucceeded")
+    private fun handleFailed() {
+        println("Fingerprint failed")
+        ObjectAnimator
+                .ofFloat(icon, "translationX", 0f, 25f, -25f, 25f, -25f, 15f, -15f, 6f, -6f, 0f)
+                .setDuration(1000)
+                .start()
+    }
 
-            override fun onAuthenticationHelp(helpMsgId: Int, helpString: CharSequence?) = logAndDismiss("onAuthenticationHelp")
-
-            override fun onAuthenticationFailed() = logAndDismiss("onAuthenticationFailed")
-
-            private fun logAndDismiss(s: String) {
-                println("Dialog result [$s]")
-                d.dismiss()
-            }
-
-        }, null)
+    override fun onPause() {
+        cs.cancel()
+        super.onPause()
     }
 }
